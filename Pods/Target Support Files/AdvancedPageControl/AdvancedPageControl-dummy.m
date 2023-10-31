cbead66c9f41b8f27c6b285e3df5c6eff005ae02
Aziz Bibitov <azizbibitov11@gmail.com>
2023-10-31 15:26:11 +0500

    excluded pods

--- Pods/Target Support Files/AdvancedPageControl/AdvancedPageControl-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_AdvancedPageControl : NSObject
-@end
-@implementation PodsDummy_AdvancedPageControl
-@end
--- Pods/Target Support Files/Pods-Altyn-Yoda/Pods-Altyn-Yoda-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Altyn_Yoda : NSObject
-@end
-@implementation PodsDummy_Pods_Altyn_Yoda
-@end
